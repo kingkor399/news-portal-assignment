3e019264f95b262985a5650c6403938d33a4b431
kingkor399 <farhanrahmanyou@gmail.com>
2022-09-04 00:47:54 +0600

    third commit

--- news.js
+++ news.js
@@ -13,41 +13,48 @@ const displayCategory = categorys =>{
         ul.classList.add('list-unstyled','p-3')
         ul.innerHTML = `
         <li>
-        <a class="nav-link active" aria-current="page" href="#">${category. category_name}</a>
+        <a onclick="newCategory('${category.category_id}')" class="nav-link active" aria-current="page" href="#">${category. category_name}</a>
         </li>
         `
         Navbar.appendChild(ul)
     })
 }
 
-const categoryNews = async() =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+const newCategory = async(news_id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`
     const res = await fetch(url)
-    const newsdata = await res.json()
-    displayCategoryNews(newsdata.data)
+    const newsData = await res.json()
+    newsCategoryDisplay(newsData.data);
 }
 
-const displayCategoryNews = newses =>{
-    const cardContainer = document.getElementById('card-container');
+const newsCategoryDisplay = newses =>{
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
     newses.forEach(news =>{
-        console.log(news);
-        const newsContainer = document.getElementById('news-container');
+        console.log(news)
         const divUser = document.createElement('div');
         divUser.classList.add('col');
         divUser.innerHTML = `
-        <div class="card h-100">
-        <img src="${news.thumbnail_url}" class="card-img-top" alt="...">
+        <div class="card">
+        <img src="${news.image_url
+        }" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details.slice(0,80)+'....'}</p>
+        </div>
+        <div class="container-fluid d-flex">
+        <div>
+        <img src="${news.author.img}" alt="" width="50" height="50" class="d-inline-block align-text-top rounded-5">
+        </div>
+        <div class="mx-2">
+        <h6>${news.author.name}</h6>
+        <p>${news.author.published_date}</p>
+        </div>
         </div>
         </div>
         `
-        newsContainer.appendChild(divUser)
+        newsContainer.appendChild(divUser);
     })
-    
 }
-
-categoryNews()
 
 categoryLoaded();
