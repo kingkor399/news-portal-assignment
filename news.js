e0b5ac057233d9ebcf9719cbe4f50a68f15903d8
kingkor399 <farhanrahmanyou@gmail.com>
2022-09-03 14:33:49 +0600

    my second comit

--- news.js
+++ news.js
@@ -6,7 +6,7 @@ const categoryLoaded = async() =>{
 }
 
 const displayCategory = categorys =>{
-    console.log(categorys)
+    // console.log(categorys)
     const Navbar = document.getElementById('Navbar');
     categorys.forEach(category =>{
         const ul = document.createElement('ul');
@@ -20,5 +20,34 @@ const displayCategory = categorys =>{
     })
 }
 
+const categoryNews = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+    const res = await fetch(url)
+    const newsdata = await res.json()
+    displayCategoryNews(newsdata.data)
+}
+
+const displayCategoryNews = newses =>{
+    const cardContainer = document.getElementById('card-container');
+    newses.forEach(news =>{
+        console.log(news);
+        const newsContainer = document.getElementById('news-container');
+        const divUser = document.createElement('div');
+        divUser.classList.add('col');
+        divUser.innerHTML = `
+        <div class="card h-100">
+        <img src="${news.thumbnail_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+        `
+        newsContainer.appendChild(divUser)
+    })
+    
+}
+
+categoryNews()
 
 categoryLoaded();
